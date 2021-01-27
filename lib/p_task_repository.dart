10fbe8f6bb3ab1c3641e5a888f462383bbfba394
Maximilian Auer <maxauer@hotmail.de>
2021-01-27 20:01:05 +0100

    add sqflite as local data storage solution

--- lib/p_task_repository.dart
+++ lib/p_task_repository.dart
@@ -2,4 +2,5 @@ library p_task_repository;
 
 export './src/exceptions/exceptions.dart';
 export './src/models/models.dart';
+export './src/sqflite/task_repository_sqflite.dart';
 export './src/task_repository.dart';
