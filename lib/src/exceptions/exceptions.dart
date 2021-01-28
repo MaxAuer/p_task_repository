04e83e4c95993c0a4ff4332d15178c748cb4e7f9
Maximilian Auer <maxauer@hotmail.de>
2021-01-28 18:58:54 +0100

    add base exception

--- lib/src/exceptions/exceptions.dart
+++ lib/src/exceptions/exceptions.dart
@@ -1,42 +1,42 @@
 /// This [Exception] should be thrown by the repository when the update of a
 /// [Task] does not complete.
-class CouldNotUpdateTask implements Exception {
+class CouldNotUpdateTask extends MessageException {
   /// The [id] of the [Task] that could not be updated.
   final String taskId;
 
   /// The [User] the [Task] should be updated for.
   final String userId;
 
-  /// Detailed description on what happened and why.
-  final String message;
-
   /// Throw this [Exception] when the [Task], or the [User] could
   /// not be found.
-  CouldNotUpdateTask(this.taskId, this.userId, this.message);
+  CouldNotUpdateTask(this.taskId, this.userId, String message) : super(message);
 }
 
 /// This [Exception] should be thrown by the repository when [Project]`s could
 /// not be fetched for the [User].
-class CouldNotFetchProjects implements Exception {
+class CouldNotFetchProjects extends MessageException {
   /// The [User] the [Project]`s should be fetched for.
   final String userId;
 
-  /// Detailed description on what happened and why.
-  final String message;
-
   /// Throw this [Exception] when the [Project]`s could not be fetched
-  CouldNotFetchProjects(this.userId, this.message);
+  CouldNotFetchProjects(this.userId, String message) : super(message);
 }
 
 /// This [Exception] should be thrown by the repository when [Task]`s could
 /// not be fetched for a [User].
-class CouldNotFetchTasks implements Exception {
+class CouldNotFetchTasks extends MessageException {
   /// The [User] the [Task]`s should be fetched for.
   final String userId;
 
+  /// Throw this [Exception] when the [Tasks]`s could not be fetched.
+  CouldNotFetchTasks(this.userId, String message) : super(message);
+}
+
+/// Base Class for [Exception]`s that include an error message
+class MessageException implements Exception {
   /// Detailed description on what happened and why.
   final String message;
 
-  /// Throw this [Exception] when the [Tasks]`s could not be fetched.
-  CouldNotFetchTasks(this.userId, this.message);
+  /// Throw this [Exception] when you want to explain what went wrong.
+  const MessageException(this.message);
 }
